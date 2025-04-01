bbe4ab3a712e25d9a3501e2c43daae77a767879a
singyuKang <rkdtlscks123@naver.com>
2025-04-01 23:53:55 +0900

    feat : renderElement, updateElement 기능 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,22 +1,31 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
+
+const containerNodeMap = new Map();
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   // 1. VNode 정규화
+  // 2. DOM 요소 생성
+  // 3. 컨테이너에 삽입
+  // 4. 이벤트 리스너 설정
   const normalizedNode = normalizeVNode(vNode);
 
-  // 2. DOM 요소 생성
-  const element = createElement(normalizedNode);
+  if (!containerNodeMap.has(container)) {
+    const element = createElement(normalizedNode);
+    container.innerHTML = "";
+    container.appendChild(element);
+  }
 
-  // 3. 컨테이너에 삽입
-  container.innerHTML = ""; // 기존 내용 제거
-  container.appendChild(element);
+  if (containerNodeMap.has(container)) {
+    const prevNode = containerNodeMap.get(container);
+    updateElement(container, normalizedNode, prevNode);
+  }
 
-  // 4. 이벤트 리스너 설정
   setupEventListeners(container);
+  containerNodeMap.set(container, normalizedNode);
 }
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,17 +1,31 @@
-// import { addEvent, removeEvent } from "./eventManager";
+import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
 function updateAttributes(target, originNewProps, originOldProps) {
-  // 달라지거나 추가된 Props를 반영
-  for (const [attr, value] of Object.entries(originNewProps)) {
-    if (originOldProps[attr] === originNewProps[attr]) continue;
-    target.setAttribute(attr, value);
+  if (!target) return;
+
+  // 기존 이벤트 제거
+  for (const [attr, value] of Object.entries(originOldProps)) {
+    if (attr.startsWith("on")) {
+      const eventType = attr.slice(2).toLowerCase();
+      removeEvent(target, eventType, value);
+    }
   }
 
-  // 없어진 props를 attribute에서 제거
-  for (const attr of Object.keys(originOldProps)) {
-    if (originNewProps[attr] !== undefined) continue;
-    target.removeAttribute(attr);
+  // 속성 및 이벤트 추가
+  for (const [attr, value] of Object.entries(originNewProps)) {
+    if (originOldProps[attr] === value) continue;
+
+    if (attr.startsWith("on")) {
+      const eventType = attr.slice(2).toLowerCase();
+      addEvent(target, eventType, value);
+    }
+    if (attr === "className") {
+      target.setAttribute("class", value);
+    }
+    if (attr !== "className" && !attr.startsWith("on")) {
+      target.setAttribute(attr, value);
+    }
   }
 }
 
@@ -21,43 +35,46 @@ export function updateElement(parentElement, newNode, oldNode, index = 0) {
 
   // 1. oldNode만 있는 경우
   if (!newNode && oldNode) {
-    return parent.removeChild(parent.childNode[index]);
+    return parentElement.removeChild(parentElement.childNodes[index]);
   }
 
   // 2. newNode만 있는 경우
   if (newNode && !oldNode) {
-    return parent.appendChild(createElement(newNode));
+    return parentElement.appendChild(createElement(newNode));
   }
 
   // 3. oldNode와 newNode 모두 text 타입일 경우
   if (typeof newNode === "string" && typeof oldNode === "string") {
     if (newNode === oldNode) return;
-    return parent.replaceChild(
+    return parentElement.replaceChild(
       createElement(newNode),
-      parent.childNodes[index],
+      parentElement.childNodes[index],
     );
   }
 
   // 4. oldNode와 newNode의 태그 이름(type)이 다를 경우
   if (newNode.type !== oldNode.type) {
-    return parent.replaceChild(
+    if (!parentElement || !parentElement.childNodes[index]) return;
+    return parentElement.replaceChild(
       createElement(newNode),
-      parent.childNodes[index],
+      parentElement.childNodes[index],
     );
   }
 
   // 5. oldNode와 newNode의 태그 이름(type)이 같을 경우
   updateAttributes(
-    parent.childNodes[index],
+    parentElement.childNodes[index],
     newNode.props || {},
     oldNode.props || {},
   );
 
   // 6. newNode와 oldNode의 모든 자식 태그를 순회하며 1 ~ 5의 내용을 반복한다.
-  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+  const newNodeChLength = newNode.children.length ?? 0;
+  const oldNodeChLenght = oldNode.children.length ?? 0;
+  const maxLength = Math.max(newNodeChLength, oldNodeChLenght);
   for (let i = 0; i < maxLength; i++) {
     updateElement(
-      parent.childNodes[index],
+      parentElement.childNodes[index],
       newNode.children[i],
       oldNode.children[i],
       i,
